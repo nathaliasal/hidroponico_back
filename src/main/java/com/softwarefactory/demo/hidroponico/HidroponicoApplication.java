4fd581d392727d8b68d6fff0a91215d2919a99ff
nathaliasal <nathaliasalguero@gmail.com>
2025-11-19 09:34:41 -0500

    Initial commit

---
+++ src/main/java/com/softwarefactory/demo/hidroponico/HidroponicoApplication.java
@@ -0,0 +1,13 @@
+package com.softwarefactory.demo.hidroponico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HidroponicoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HidroponicoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/softwarefactory/demo/hidroponico/HidroponicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwarefactory.demo.hidroponico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HidroponicoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
